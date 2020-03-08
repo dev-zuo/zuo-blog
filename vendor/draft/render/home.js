8a1621dbc3b76b8b6523e2a6649b0903f8be6f86
guoqzuo <guoqzuo@gmail.com>
2020-03-08 21:40:39 +0800

    根据备案要求,在主页底部增加备案链接信息

--- vendor/draft/render/home.js
+++ vendor/draft/render/home.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 
 function render(config, globalScript) {
-  let {indexConfig, title} = config
+  let {indexConfig, title, footer} = config
 
   // 生成linkList
   let linkList = ''
@@ -30,9 +30,12 @@ function render(config, globalScript) {
         <link rel="shortcut icon" href="/images/favicon.ico">
         ${config._isGlobalCssFileExists ? '<link rel="stylesheet" href="/lib/global.css">' : ''}
         <style>
+          body {
+            margin: 0;
+          }
           article {
             position: absolute;
-            top: 30%;
+            top: 33%;
             left: 50%;
             transform: translate(-50%, -50%);
           }
@@ -41,19 +44,32 @@ function render(config, globalScript) {
             font-weight:lighter;
             text-align:center;
             width: 300px;
+            color: #333;
           }
           .link {
             display: flex;
             justify-content: space-around;
             margin: 20px 0;
           }
-          .link a {
+          .link a, .footer_content a{
             text-decoration: none;
             color: #25b864;
             font-weight: normal;
           }
           .img-div {
             text-align:center;margin-bottom:10px;
+          }
+          .footer_content {
+            position: fixed;
+            bottom: 15px;
+            display: flex;
+            flex-wrap:wrap;
+            width: 100%;
+            box-sizing: border-box;
+            padding: 0 9%;
+            justify-content: space-between;
+            font-size: 14px;
+            color: #666;
           }
         </style>
       </head>
@@ -67,6 +83,12 @@ function render(config, globalScript) {
             ${linkList}
           </div>
         </article>
+        <footer>
+          <div class="footer_content">
+            <div class="footer_right">${footer.left}</div>
+            <div>${footer.right}</div>
+          </div>
+        </footer>
         ${config._isGlobalJsFileExists ? '<script src="/lib/global.js"></script>' : ''}
         <script>
           ${globalScript}
